6aa85c0080e4db731b4e77e38ed0cf6fdb7b1bcc
Даниил Романюк <159701642+coole-c@users.noreply.github.com>
2025-12-06 16:57:30 +0300

    Update functions.cpp

--- trunk/as06516/task_02/src/functions.cpp
+++ trunk/as06516/task_02/src/functions.cpp
@@ -2,14 +2,15 @@
 #include <cmath>
 
 const double a = 0.92;
-const double b = 0.08;
+const double b_lin = 0.08;    
+const double b_non = 0.005;   
 const double c = 0.07;
 const double d = 0.04;
 
 double linear(double yt, double ut) {
-    return a * yt + b * ut;
+    return a * yt + b_lin * ut;
 }
 
 double nonlinear(double yt, double yt1, double ut, double ut1) {
-    return a * yt - b * yt1 * yt1 + c * ut + d * sin(ut1);
+    return a * yt - b_non * yt1 * yt1 + c * ut + d * std::sin(ut1);  
 }
